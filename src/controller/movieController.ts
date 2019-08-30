299248db40c328569e7089f2c805246e104b3ece
Abdal Pasha <abdalpasha1995@gmail.com>
2019-08-30 20:07:20 +0530

    corrections

--- src/controller/movieController.ts
+++ src/controller/movieController.ts
@@ -4,22 +4,22 @@ import { MovieServices } from "../Services.ts/movieServices";
 export class movieController{
     public async createMovie(req:express.Request, res:express.Response){
         let result = await MovieServices.createMovie(req,res);
-        return result;
+        res.json(result)
     }
 
     public async getAllMovies(req:express.Request, res:express.Response){
         let result = await MovieServices.getAllMovies(req,res);
-        return result;
+        res.json(result)
     }
 
     public async getMovieById(req:express.Request, res:express.Response){
         let result = await MovieServices.getMovieById(req,res);
-        return result;
+        res.json(result)
     }
 
     public async updateMovieById(req:express.Request, res:express.Response){
         let result = await MovieServices.updateMovieById(req,res);
-        return result;
+        res.json(result)
     }
     
 }
